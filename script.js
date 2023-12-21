d132e4c3b6dbf1d3143f79c76f2b4b56aad322f6
divyadeep10 <divyadeepsundriyal@gmail.com>
2023-12-21 20:26:54 +0530

    added comments

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+     // Getting references to DOM elements
     const searchicon = document.getElementById('search_icon');
     const searchbar = document.getElementById('googlesearchbar_input');
     const searchbutton = document.getElementById('button1');
@@ -8,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const signInbutton = document.querySelector('.signinbutton');
     const clearsearchbar = document.getElementById('clear_searchicon');
 
-
+    // Adding event listeners
     searchicon.addEventListener('click', togglesearchbar);
     searchbutton.addEventListener('click', handlesearch);
     gmailbar.addEventListener('click', handlegmailclick);
@@ -17,20 +19,24 @@ document.addEventListener('DOMContentLoaded', function () {
     signInbutton.addEventListener('click', handlesignin);
     clearsearchbar.addEventListener('click', clearbar);
 
+    // setting to make cross sign not avaliable during no input
     clearsearchbar.style.display='none';
 
+    // adding event listener to show/hide cross sign based on input value
     searchbar.addEventListener('input', function () {
         clearsearchbar.style.display = searchbar.value.trim() !== '' ? 'block' : 'none';
     });
-
+    
+    // Adding click event listener to clear search icon
     clearsearchbar.addEventListener('click', clearbar);
 
+    // Function to clear the search bar
     function clearbar(){
         searchbar.value ='';
         clearsearchbar.style.display='none'; 
     }
 
-
+    // Adding keyup event listener to the search bar for handling Enter key press
     searchbar.addEventListener('keyup', function (event) {
         if (event.key === 'Enter') {
             handleSearch();
@@ -38,12 +44,14 @@ document.addEventListener('DOMContentLoaded', function () {
         
     });
 
+    // Function to toggle the search bar
     function togglesearchbar() {
         searchbar.classList.toggle('active');
         searchbar.focus();
        
     }
 
+    // Function to handle the searched input
     function handlesearch() {
         const searchterm = searchbar.value.trim();
         if (searchterm !== '') {
@@ -54,22 +62,25 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
 
-
+     // Function to handle Gmail click
     function handlegmailclick() {
         alert("Gmail clicked!");
         
     }
 
+    // Function to handle Images click
     function handleimageclick() {
         alert("Images clicked!");
         
     }
 
+    // Function to handle Grid click
     function handlegridclick() {
         alert("Grid clicked!");
         
     }
 
+     // Function to handle Sign In click
     function handlesignin() {
         alert("Sign in clicked!");
         // window.location.href = "https://www.google.com";
